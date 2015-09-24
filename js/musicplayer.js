fbdef9e0cd8af2df81e9c91c47ba88537e0ffd27
Payne <hu_yang110@163.com>
2015-09-24 13:50:36 +0800

    logical structure readjust and bugs fix

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -8,7 +8,8 @@
 
 	//Edit your playlist here.
 	var playList = [
-		{'artist':'','musicName':'','musicURL':'','albumPic':'',}
+		{'artist':'','musicName':'','musicURL':'','albumPic':'',},
+	
 	];
  
 	var loadFontAwesome = true;//If you set this to false ,then you should download the Font Awesome CSS and add it to your HTML document manually.
@@ -17,81 +18,74 @@
  
 		var _movetitle = false;
 		
-		if(loadFontAwesome)loadCss("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");//Font Awesome CSS by CDN
+		if(loadFontAwesome)cssLoad("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");//Font Awesome CSS by CDN
 	 
 		playerAdd(playerInit);	
 		
-		audio.addEventListener('ended',function(){
-			toNext();
-		},false);
+		audio.onplaying = function(){
+			_playstatus = 'playing';
+			cdPlay();
+		}
 		
-		function titleMove(node,movelength){
-			if(!_movetitle)return false;
-			var nodeObj = document.querySelector(node);
-			var mLeft = 0-nodeObj.offsetLeft;
-			if(mLeft>=movelength)return false;
-			mLeft += 1;
-			nodeObj.style.marginLeft = '-'+mLeft+'px';
-			return true;
-		}		
+		audio.onpause = function(){
+			_playstatus = 'pause';
+			cdPause();
+		}
+		
+		audio.onended = function(){
+			toNext();
+		};
+
+		audio.onerror = function(){ 
+			console.log('error');
+			cdPause();
+		}	
+
+		audio.onwaiting = function(){//while the buffering cause a pause
+			console.log('waiting');
+		}
+				
 		
 		document.querySelector('#music-title').onmouseover = function(){			
-			_movetitle = true;		
+			_movetitle = true;	
+			inernal = setInterval(function(){titleMove('#music-title-text')},20);			
 		};		
 	
 		document.querySelector('#music-title').onmouseout = function(){
 			_movetitle = false;
 			titleReset();
+			clearInterval(inernal);
 		};		
 		
-		document.querySelector('#player-btn-play').onclick = function(){
+		document.querySelector('#player-btn-play').onmousedown = function(){
 			if(_playstatus=='pause'){
 				toPlay();
-			}else if(_playstatus=='running'){
+			}else if(_playstatus=='playing'){
 				toPause();
 			}	
 		};
 		
-		document.querySelector('#player-btn-backward').onclick = function(){
+		document.querySelector('#player-btn-backward').onmousedown = function(){
 			toPrev();		
 		};
 		
-		document.querySelector('#player-btn-forward').onclick = function(){
+		document.querySelector('#player-btn-forward').onmousedown = function(){
 			toNext();		
 		};
 		
-		function titleReset(){
-			document.querySelector('#music-title-text').style.marginLeft = '0px';
-		}
-		
-		function toPlay(){
-			document.querySelector('#player-disk').style.animationPlayState = 'running';
-			document.querySelector('#player-disk').style.WebkitAnimationPlayState  = 'running';
-			document.querySelector('#player-btn-play').innerHTML = '<i class="fa fa-pause fa-lg textshadow"></i>';
-			_playstatus = 'running';
-			audio.play();
-		}
-		
-		function toPause(){
-			document.querySelector('#player-disk').style.animationPlayState = 'paused';
-			document.querySelector('#player-disk').style.WebkitAnimationPlayState  = 'paused';
-			document.querySelector('#player-btn-play').innerHTML = '<i class="fa fa-play fa-lg textshadow"></i>';
-			_playstatus = 'pause';
-			audio.pause();
-		}
 		
 		function playerAdd(callback){
-			html = '';
+			var	html = '';
 			html += '<div  class="music-player" id="music-player">';
 			html += '<div class="i-circle" id="player-disk">';
 			html += '<div class="i-circle1"><div class="i-circle2"></div></div>';
 			html += '</div>';
-			html += '<div class="music-title" id="music-title">';
+			html += '<div class="music-title select-disable" id="music-title">';
 			html += '<span  id="music-title-text"></span>';
 			html += '</div>';
-			html += '<div class="music-controlbar">';
+			html += '<div class="music-controlbar select-disable">';
 			html += '<div class="player-btn-backward" id="player-btn-backward" ><i class="fa fa-step-backward fa-lg textshadow"></i></div>';
-			html += '<div class="player-btn-play" id="player-btn-play" ></div>';
+			html += '<div class="player-btn-play" id="player-btn-play" ><i class="fa fa-play fa-lg textshadow"></i></div>';
 			html += '<div class="player-btn-forward" id="player-btn-forward" ><i class="fa fa-step-forward fa-lg textshadow"></i></div>';
 			html += '</div>';
 			html += '</div>';
@@ -108,13 +102,21 @@
 			musicNum = playList.length;
 			index = 0;
 			prepareToPlay('init');
-			var moveLength = moveLengthGet();
-			if(moveLength>0){setInterval(function(){titleMove('#music-title-text',moveLength)},20);}
-			document.querySelector('#player-disk').style.animationPlayState = 'paused';
-			document.querySelector('#player-disk').style.WebkitAnimationPlayState = 'paused';
-			document.querySelector('#player-btn-play').innerHTML = '<i class="fa fa-play fa-lg textshadow"></i>';
-			preloadImg();
+			moveLength = movelengthGet();
+			albumPreload();
 		}
+		
+		function titleReset(){
+			document.querySelector('#music-title-text').style.marginLeft = '0px';
+		}
+		
+		function toPlay(){
+			audio.play();
+		}
+		
+		function toPause(){			
+			audio.pause();
+		}		
 		
 		function toNext(){
 			++index;
@@ -128,21 +130,32 @@
 			prepareToPlay();	
 		}
 		
-		function prepareToPlay(status){
-			document.querySelector("#song").src = playList[index].musicURL;
-			document.querySelector("#player-disk").style.background = '#fff url('+playList[index].albumPic+') no-repeat center / 100% 100%';
-			if(typeof(status) == "undefined")audio.load();
-			document.querySelector('#music-title-text').innerText = playList[index].musicName+" - "+playList[index].artist;
-			if(_playstatus=='running')audio.play();
+		function prepareToPlay(mode){
+			resourceLoad();
+			moveLength = movelengthGet();
+			if(mode == 'init')return;
+			audio.load();
+			if(_playstatus=='pause')return;	
+			audio.play();
 		}	
-				
-		function moveLengthGet(){
-			var titlewidth = document.querySelector('#music-title').offsetWidth;
-			var textwidth = document.querySelector('#music-title-text').offsetWidth;
+		
+		function titleMove(node){
+			if(!_movetitle)return;
+			if(moveLength<=0)return;
+			var nodeObj = document.querySelector(node);
+			var mLeft = 0-nodeObj.offsetLeft;
+			if(mLeft>=moveLength)return;
+			mLeft += 1;
+			nodeObj.style.marginLeft = '-'+mLeft+'px';
+		}
+		
+		function movelengthGet(){
+			titlewidth = document.querySelector('#music-title').offsetWidth;
+			textwidth = document.querySelector('#music-title-text').offsetWidth;
 			return textwidth - titlewidth;
 		}
 		
-		function preloadImg(){
+		function albumPreload(){
 			images = new Array();			
 			for (var i = 0; i < playList.length; i++) {
 				images[i] = new Image();
@@ -150,12 +163,28 @@
 			}      
 		}
 		
-		function loadCss(url){ 
+		function resourceLoad(){
+			audio.src = playList[index].musicURL;
+			document.querySelector("#player-disk").style.background = '#fff url('+playList[index].albumPic+') no-repeat center / 100% 100%';
+			document.querySelector('#music-title-text').innerHTML = playList[index].musicName+" - "+playList[index].artist;
+		}		
+		
+		function cssLoad(url){ 
 			var link = document.createElement("link"); 
 			link.type = "text/css"; 
 			link.rel = "stylesheet"; 
 			link.href = url; 
-			document.querySelectorAll("head")[0].appendChild(link); 
-		}; 
+			document.querySelector("head").appendChild(link); 
+		} 
+		
+		function cdPlay(){
+			document.querySelector('#player-disk').style.animationPlayState = 'running';
+			document.querySelector('#player-btn-play').innerHTML = '<i class="fa fa-pause fa-lg textshadow"></i>';			
+		}
+		
+		function cdPause(){
+			document.querySelector('#player-disk').style.animationPlayState = 'paused';
+			document.querySelector('#player-btn-play').innerHTML = '<i class="fa fa-play fa-lg textshadow"></i>';		
+		}
 
 	} 
