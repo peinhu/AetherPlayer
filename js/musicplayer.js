64ec1b9ace1079060aab26d0c8057bca0dd00761
Payne <hu_yang110@163.com>
2015-09-28 15:34:18 +0800

    add playmode control

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -13,15 +13,16 @@
 	
 	//Config your player here.
 	var playerConfig = {
-		position:'leftbottom',//[lefttop|leftbottom|righttop|rightbottom] The position of audio player.
-		fontFamily:'arial,sans-serif',//[FONTFAMILY] The fonts of your text.
-		loadFontAwesome:true,//[true|false] Use the online Font Awesome CSS. If you set this to false ,then you should download the Font Awesome CSS and add it to your HTML document manually.
+		position : 'left',//[lefttop|leftbottom|righttop|rightbottom] The position of audio player.
+		fontFamily : 'arial,sans-serif',//[FONTFAMILY] The fonts of your text.
+		loadFontAwesome : true,//[true|false] Use the online Font Awesome CSS. If you set this to false ,then you should download the Font Awesome CSS and add it to your HTML document manually.
+		playMode : 'order',//[order|repeat|random] Play mode by default.
 	};
 	
 	
 	window.onload = function() {
  
-		var _movetitle = false;
+		var _movetitle = false,_playstatus = 'pause',_playmode;
 		
 		if(playerConfig.loadFontAwesome)cssLoad("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");//Font Awesome CSS by CDN
 	 
@@ -36,7 +37,7 @@
 		}
 		
 		audio.onended = function(){
-			toNext();
+			musicNext();
 		};
 
 		audio.onerror = function(){ 
@@ -61,35 +62,43 @@
 		
 		document.querySelector('#player-btn-play').onmousedown = function(){
 			if(_playstatus=='pause'){
-				toPlay();
+				musicPlay();
 			}else if(_playstatus=='playing'){
-				toPause();
+				musicPause();
 			}	
 		};
 		
 		document.querySelector('#player-btn-backward').onmousedown = function(){
-			toPrev();		
+			musicPrev();		
 		};
 		
 		document.querySelector('#player-btn-forward').onmousedown = function(){
-			toNext();		
+			musicNext();		
 		};
 		
+		
+		document.querySelector('#music-playmode').onmousedown = function(){
+			playModeChange();		
+		};
+
 		
 		function playerAdd(callback){
 			var	html = '';
 			html += '<div  class="music-player" id="music-player">';
+			
 			html += '<div class="i-circle" id="player-disk">';
 			html += '<div class="i-circle1"><div class="i-circle2"></div></div>';
 			html += '</div>';
 			html += '<div class="music-title select-disable" id="music-title">';
 			html += '<span  id="music-title-text"></span>';
 			html += '</div>';
+			html += '<div class="music-playmode select-disable" id="music-playmode"></div>'
 			html += '<div class="music-controlbar select-disable">';
 			html += '<div class="player-btn-backward" id="player-btn-backward" ><i class="fa fa-step-backward fa-lg textshadow"></i></div>';
 			html += '<div class="player-btn-play" id="player-btn-play" ><i class="fa fa-play fa-lg textshadow"></i></div>';
 			html += '<div class="player-btn-forward" id="player-btn-forward" ><i class="fa fa-step-forward fa-lg textshadow"></i></div>';
 			html += '</div>';
+			
 			html += '</div>';
 			html += '<audio id="songs" preload="none"><source src="" type="audio/mpeg" id="song">This technique is not supported by this ancient browser.</audio>';
 			var newNode = document.createElement("div");
@@ -99,13 +108,12 @@
 		}
 		
 		function playerInit(){
+
 			configLoad();	
 			audio = document.querySelector("#songs");
-			_playstatus = 'pause';
 			musicNum = playList.length;
 			index = 0;
 			prepareToPlay('init');
-			moveLength = movelengthGet();
 			albumPreload();
 		}
 		
@@ -114,25 +122,33 @@
 			document.querySelector('#music-title-text').style.marginLeft = '0px';
 		}
 		
-		function toPlay(){
+		function musicPlay(){
 			_playstatus = 'playing';
 			audio.play();
 		}
 		
-		function toPause(){
+		function musicPause(){
 			_playstatus = 'pause';
 			audio.pause();
 		}		
 		
-		function toNext(){
-			++index;
-			if(index>musicNum-1)index=0;
+		function musicNext(){
+			switch(_playmode){
+				case 'order' : ++index;if(index>musicNum-1)index = 0;
+				case 'repeat' : break;
+				case 'random': index = randomIndexGet();break;
+				default : break;
+			}
 			prepareToPlay();
 		}
 		
-		function toPrev(){
-			--index;
-			if(index<0)index=musicNum-1;
+		function musicPrev(){
+			switch(_playmode){
+				case 'order' : --index;if(index<0)index = musicNum-1;
+				case 'repeat' : break;
+				case 'random' : index = randomIndexGet();break;
+				default : break;
+			}
 			prepareToPlay();	
 		}
 		
@@ -200,12 +216,13 @@
 		function configLoad(){
 			positionSet();
 			fontFamilySet();
+			playModeSet();
 		}
 		
 		
 		//set the position of audio player
 		function positionSet(){
-			var left,right,top,bottom;
+			var left='auto',top='auto',bottom='auto',right='auto';
 			switch(playerConfig.position){
 				case 'lefttop': left = '-100px';right = 'auto';top = '-100px';bottom = 'auto';break;
 				case 'leftbottom': left = '-100px';right = 'auto';top = 'auto';bottom = '-100px';break;
@@ -214,14 +231,44 @@
 				default :break;
 			}
 			document.querySelector('#music-player').style.left = left;
-			document.querySelector('#music-player').style.right = right;
 			document.querySelector('#music-player').style.top = top;
+			document.querySelector('#music-player').style.right = right;
 			document.querySelector('#music-player').style.bottom = bottom;
 			document.querySelector('#music-player').className += " "+playerConfig.position;
 		}
 		
 		function fontFamilySet(){
 			document.querySelector('#music-title-text').style.fontFamily = playerConfig.fontFamily;
+		}
+		
+		function playModeSet(playmode){
+			if(typeof(playmode) == "undefined"){
+				playmode = playerConfig.playMode;
+			}
+			switch(playmode){
+				case 'order':_playmode = 'order';document.querySelector('#music-playmode').innerHTML = '<i class="fa fa-list textshadow"></i>';break;
+				case 'repeat':_playmode = 'repeat';document.querySelector('#music-playmode').innerHTML = '<i class="fa fa-retweet textshadow"></i>';break;
+				case 'random':_playmode = 'random';document.querySelector('#music-playmode').innerHTML = '<i class="fa fa-random textshadow"></i>';break;
+				default:break;
+			}		
+		}
+		
+		function playModeChange(){
+			var playmodeArray = new Array('order','repeat','random'),playmodeArray_index;
+			for(var i=0;i<playmodeArray.length;i++){
+				if(playmodeArray[i] == _playmode){playmodeArray_index = i;break;}		
+			}
+			++playmodeArray_index;
+			if(playmodeArray_index>playmodeArray.length-1)playmodeArray_index = 0;
+			playModeSet(playmodeArray[playmodeArray_index]);		
+		}
+		
+		function randomIndexGet(){
+			var randomIndex = index;
+			while(randomIndex==index){ //make sure to get the different index
+				randomIndex = Math.floor(Math.random()*musicNum);
+			}
+			return randomIndex;
 		}
 
 	} 
