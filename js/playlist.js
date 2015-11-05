8640b208a9a49275b7b292ca3c5c62bc7e9956a3
Payne <hu_yang110@163.com>
2015-11-05 13:43:15 +0800

    solve issues in playlist

--- js/playlist.js
+++ js/playlist.js
@@ -1,3 +1,7 @@
-	//Edit your playlist here.
-	var playList = [];
-	//var playList = [{'artist':'','songName':'','songURL':'','songCover':''},]
+	/**
+	 * Edit your playlist here.
+	 * Notice:This variable will take effect only if the dataStorage param is set to "file" in configuration.
+	 * e.g. var aetherplayer_playList_file = [{'artist':'','songName':'','songURL':'','songCover':''},]
+	 */
+
+	var aetherplayer_playList_file = [];
